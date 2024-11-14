227944ae8ee658d139ae4fe442bc10ffeb3252cd
Chelsea Wright <chelsea.wr@gmail.com>
2024-11-14 16:41:27 -0500

    remove fa packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 Amplify.configure(outputs);
 
 @Component({
@@ -10,21 +9,10 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, FontAwesomeModule],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   shiningRed = `rgb(152, 31, 36)`;
   shiningOrange = `rgb(223, 95, 24)`;
   title = 'cdev_ng';
-
-  constructor(breakpointObserver: BreakpointObserver) {
-    breakpointObserver
-      .observe([Breakpoints.HandsetPortrait])
-      .subscribe((result) => {
-        if (result.matches) {
-          // TODO utilize for fancy mobile scroll animation
-          console.log('result matches mobile');
-        }
-      });
-  }
 }
