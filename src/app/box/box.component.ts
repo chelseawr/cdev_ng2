677072b6fc8d1e520649a640bf3493a420a23a96
Chelsea Wright <chelsea.wr@gmail.com>
2024-11-15 19:55:35 -0500

    pretty buttons

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -4,16 +4,22 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { ContactButtonComponent } from '../contact-button/contact-button.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-box',
   standalone: true,
-  imports: [AdjectiveWheelComponent, ContactButtonComponent, FontAwesomeModule],
+  imports: [
+    AdjectiveWheelComponent,
+    MatButtonModule,
+    ContactButtonComponent,
+    FontAwesomeModule,
+  ],
   templateUrl: './box.component.html',
   styleUrl: './box.component.scss',
 })
 export class BoxComponent {
-  // change source path to Amplify hosted file /assets
+  // TODO change source path to Amplify hosted file /assets
   sourcePath =
     'https://github.com/chelseawr/cdev_ng/blob/master/src/assets/May2024-resume.pdf';
   fileName = 'May2024-resume.pdf';
