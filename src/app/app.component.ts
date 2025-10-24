6b80a78d79ee2b22ec9dce99244a4c43754673be
Chelsea Wright <chelsea.wr@gmail.com>
2025-10-23 21:57:18 -0400

    remove amplify backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { Amplify } from 'aws-amplify';
-import outputs from '../../amplify_outputs.json';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BoxComponent } from './box/box.component';
 import { bootstrapApplication } from '@angular/platform-browser';
-Amplify.configure(outputs);
 
 @Component({
   selector: 'app-root',
