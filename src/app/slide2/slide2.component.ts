93b085a827df278a1cbdeceefd5d5480c3e2aa8b
Chelsea Wright <chelsea.wr@gmail.com>
2025-10-27 12:57:31 -0400

    responsive breakpoints on slide2

--- src/app/slide2/slide2.component.ts
+++ src/app/slide2/slide2.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule, NgClass } from '@angular/common';
 @Component({
   selector: 'app-slide2',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './slide2.component.html',
   styleUrl: './slide2.component.scss',
 })
-export class Slide2Component {}
+export class Slide2Component implements OnInit {
+  isPhonePortrait = false;
+  constructor(private responsive: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.responsive.observe(Breakpoints.HandsetPortrait).subscribe((result) => {
+      this.isPhonePortrait = false;
+      if (result.matches) this.isPhonePortrait = true;
+    });
+  }
+}
