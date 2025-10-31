4d30db1e856bd8c82bab3c0b21ad718c6e8872f8
Chelsea Wright <chelsea.wr@gmail.com>
2025-10-30 20:35:34 -0400

    almost done with experience page mobile

--- src/app/slide3/slide3.ts
+++ src/app/slide3/slide3.ts
@@ -1,6 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faArrowRight,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 import { Role, ROLES } from './roles.data';
 import { ResponsiveService } from '../responsive.service';
 
@@ -16,9 +23,20 @@ export class Slide3 {
 
   faArrowLeft = faArrowLeft;
   faArrowRight = faArrowRight;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
+  faChevronDown = faChevronDown;
   roles: Role[] = ROLES;
   openId: string | null = null;
 
+  iconFor(i: number, role: Role): IconDefinition {
+    const isOpen = this.openId === role.id;
+    return isOpen
+      ? faChevronDown
+      : i % 2 === 0
+      ? faChevronRight
+      : faChevronLeft;
+  }
   onToggle(id: string, ev: MouseEvent) {
     ev.preventDefault();
     this.openId = this.openId === id ? null : id;
