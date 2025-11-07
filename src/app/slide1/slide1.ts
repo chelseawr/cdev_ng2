da29bce97a75d57391f3d4aa0772e83ad0c2f1cd
Chelsea Wright <chelsea.wr@gmail.com>
2025-11-06 23:26:02 -0500

    first slide overhaul

--- src/app/slide1/slide1.ts
+++ src/app/slide1/slide1.ts
@@ -1,30 +1,34 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatButtonModule } from '@angular/material/button';
-import { AdjectiveWheel } from '../adjective-wheel/adjective-wheel';
-import { ContactButton } from '../contact-button/contact-button';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faChevronDown, faFilePdf } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronDown,
+  faCircleChevronRight,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons';
 import { ResponsiveService } from '../responsive.service';
 
 @Component({
   selector: 'app-slide1',
   standalone: true,
-  imports: [FontAwesomeModule, AdjectiveWheel, MatButtonModule, ContactButton],
+  imports: [FontAwesomeModule, MatButtonModule],
   templateUrl: './slide1.html',
   styleUrl: './slide1.scss',
 })
 export class Slide1 {
   constructor(public responsive: ResponsiveService) {}
-  sourcePath =
-    'https://public-cdev.s3.us-east-2.amazonaws.com/ChelseaMay2025resume.pdf';
-  fileName = 'ChelseaMay2025resume.pdf';
 
-  faLinkedin = faLinkedin;
-  faGithub = faGithub;
-  faFilePdf = faFilePdf;
   faArrow = faChevronDown;
 
+  links = {
+    contact: 'mailto:chelsearmay51@gmail.com',
+    github: 'https://github.com/chelseawr',
+    linkedin: 'https://www.linkedin.com/in/chelseawr/',
+    fileName: 'ChelseaMay2025resume.pdf',
+    resume:
+      'https://public-cdev.s3.us-east-2.amazonaws.com/ChelseaMay2025resume.pdf',
+  };
   scrollTo(id: string) {
     const el = document.getElementById(id);
     if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
